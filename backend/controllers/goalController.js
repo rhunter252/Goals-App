2e222264fff3283640a8cf558b8ee833b78102dd
Randy Hunter <randyh252@gmail.com>
2023-01-11 13:16:32 -0500

    JWT authentication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,15 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals" });
+  const goals = await Goal.find({ user: req.user.id });
+
+  res.status(200).json(goals);
 });
 
 // @desc Set Goal
@@ -15,22 +20,71 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
+  const goal = await Goal.create({ text: req.body.text, user: req.user.id });
 
-  res.status(200).json({ message: "Set goals" });
+  res.status(200).json(goal);
 });
 
 // @desc Update Goal
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc Delete Goal
 // @route DElETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
---
+++ backend/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Please add a email"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    password: { type: String, required: [true, "Please add a password"] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
